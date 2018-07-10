f78928de120594917d3706b5e54904e8ce15efc1
Robert Lemmens <robert_lemmens@hotmail.com>
2018-07-10 10:41:06 +0200

    started Either type

---
+++ src/main/java/nl/codebulb/functionaltypes/Either.java
@@ -0,0 +1,20 @@
+package nl.codebulb.functionaltypes;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public abstract class Either<L,R> implements Functor<L,Either<?,?>>{ //Gebruik Monad instead?
+
+    private Either() {}
+
+    abstract <T> T map(Function<? super L, ? extends T> lFunc, Function<? super R, ? extends T> rFunc);
+
+    public void apply(Consumer<? super L> f1, Consumer<? super R> f2){
+        map(consume(f1), consume(f2));
+    }
+
+    private <T> Function<T, Void> consume(Consumer<T> c) {
+        return t -> {c.accept(t); return null;};
+    }
+
+}
--- src/main/java/nl/codebulb/functionaltypes/Monad.java
+++ src/main/java/nl/codebulb/functionaltypes/Monad.java
@@ -4,6 +4,8 @@ import java.util.function.Function;
 
 public interface Monad<F, M extends Monad<?,?>> extends Functor<F,M> {
 
+    M of();
+
     M flatMap(Function<F,M> f);
 
     String toString();
